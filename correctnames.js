d4e53e2f79e3667e163efcefa11b33b53d9d0b1b
enjoni <jo.wiederkehr@gmail.com>
2017-06-02 09:30:34 +0200

    new one

---
+++ correctnames.js
@@ -0,0 +1,9 @@
+/**
+ * Created by valentinberger on 30/05/17.
+ */
+function removeDashes() {
+    console.log("removeDash")
+    document.getElementById("node-Bio-Peperoni").innerHTML = '<text dx="8" dy=".31em" text-anchor="start">Bio Peperoni</text>';
+}
+
+setTimeout(removeDashes, 2000);
---
+++ js/main.js
@@ -0,0 +1,375 @@
+$(document).ready(function () {
+	console.log("reload is ready!");
+
+
+	$('.info').mouseover(
+		function () {
+			$("#hide").stop().show(150)
+		});
+
+	$('.info').mouseout(
+		function () {
+			$("#hide").stop().hide(150)
+		});
+
+
+	setTimeout(function () {
+
+		//Franken
+		$('#node-weniger-als-ein-CHF').mouseover(
+			function () {
+				$("#1CHF").stop().show(150)
+			});
+
+		$('#node-weniger-als-ein-CHF').mouseout(
+			function () {
+				$("#1CHF").stop().hide(150)
+			});
+
+		$('#node-mehr-als-vier-CHF').mouseover(
+			function () {
+				$("#3CHF").stop().show(150)
+			});
+
+
+		$('#node-mehr-als-vier-CHF').mouseout(
+			function () {
+				$("#3CHF").stop().hide(150)
+			});
+
+		$('#node-ein-bis-vier-CHF').mouseover(
+			function () {
+				$("#2CHF").stop().show(150)
+			});
+
+		$('#node-ein-bis-vier-CHF').mouseout(
+			function () {
+				$("#2CHF").stop().hide(150)
+			});
+
+		//Anzahl
+		$('#node-eins-gekauft').mouseover(
+			function () {
+				$("#eins").stop().show(150)
+			});
+
+		$('#node-eins-gekauft').mouseout(
+			function () {
+				$("#eins").stop().hide(150)
+			});
+
+		$('#node-zwei-bis-drei').mouseover(
+			function () {
+				$("#zwei").stop().show(150)
+			});
+
+
+		$('#node-zwei-bis-drei').mouseout(
+			function () {
+				$("#zwei").stop().hide(150)
+			});
+
+		$('#node-mehr-als-drei').mouseover(
+			function () {
+				$("#drei").stop().show(150)
+			});
+
+		$('#node-mehr-als-drei').mouseout(
+			function () {
+				$("#drei").stop().hide(150)
+			});
+
+		
+		//Lebensabschnitt
+		$('#node-Studium').mouseover(
+			function () {
+				$("#stud").stop().show(150)
+			});
+
+		$('#node-Studium').mouseout(
+			function () {
+				$("#stud").stop().hide(150)
+			});
+
+		$('#node-Arbeit').mouseover(
+			function () {
+				$("#arb").stop().show(150)
+			});
+
+
+		$('#node-Arbeit').mouseout(
+			function () {
+				$("#arb").stop().hide(150)
+			});
+
+		$('#node-Zivildienst').mouseover(
+			function () {
+				$("#zivi").stop().show(150)
+			});
+
+		$('#node-Zivildienst').mouseout(
+			function () {
+				$("#zivi").stop().hide(150)
+			});
+
+		//Produkte
+
+		$('#node-Bio-Peperoni').mouseover(
+			function () {
+				$("#pr1").stop().show(150)
+			});
+
+
+		$('#node-Bio-Peperoni').mouseout(
+			function () {
+				$("#pr1").stop().hide(150)
+			});
+
+
+		$('#node-Butter-Laugenbrezel').mouseover(
+			function () {
+				$("#pr2").stop().show(150)
+			});
+
+		$('#node-Butter-Laugenbrezel').mouseout(
+			function () {
+				$("#pr2").stop().hide(150)
+			});
+
+		$('#node-Erdbeeren').mouseover(
+			function () {
+				$("#pr3").stop().show(150)
+			});
+
+		$('#node-Erdbeeren').mouseout(
+			function () {
+				$("#pr3").stop().hide(150)
+			});
+
+		$('#node-Frischback-Pita').mouseover(
+			function () {
+				$("#pr4").stop().show(150)
+			});
+
+		$('#node-Frischback-Pita').mouseout(
+			function () {
+				$("#pr4").stop().hide(150)
+			});
+
+		$('#node-Ice-Tea-Zitrone').mouseover(
+			function () {
+				$("#pr5").stop().show(150)
+			});
+
+		$('#node-Ice-Tea-Zitrone').mouseout(
+			function () {
+				$("#pr5").stop().hide(150)
+			});
+
+		$('#node-Jumpys-Paprika').mouseover(
+			function () {
+				$("#pr6").stop().show(150)
+			});
+
+		$('#node-Jumpys-Paprika').mouseout(
+			function () {
+				$("#pr6").stop().hide(150)
+			});
+
+		$('#node-Kartoffeln-mehligkochend').mouseover(
+			function () {
+				$("#pr7").stop().show(150)
+			});
+
+		$('#node-Kartoffeln-mehligkochend').mouseout(
+			function () {
+				$("#pr7").stop().hide(150)
+			});
+
+		$('#node-Kinder-Überraschung').mouseover(
+			function () {
+				$("#pr8").stop().show(150)
+			});
+
+		$('#node-Kinder-Überraschung').mouseout(
+			function () {
+				$("#pr8").stop().hide(150)
+			});
+
+		$('#node-Kuchenglasur-hell').mouseover(
+			function () {
+				$("#pr9").stop().show(150)
+			});
+
+		$('#node-Kuchenglasur-hell').mouseout(
+			function () {
+				$("#pr9").stop().hide(150)
+			});
+
+		$('#node-Le-Gruyère').mouseover(
+			function () {
+				$("#pr10").stop().show(150)
+			});
+
+		$('#node-Le-Gruyère').mouseout(
+			function () {
+				$("#pr10").stop().hide(150)
+			});
+
+		$('#node-Lime').mouseover(
+			function () {
+				$("#pr11").stop().show(150)
+			});
+
+		$('#node-Lime').mouseout(
+			function () {
+				$("#pr11").stop().hide(150)
+			});
+
+		$('#node-MBudget-Blätterteig').mouseover(
+			function () {
+				$("#pr12").stop().show(150)
+			});
+
+		$('#node-MBudget-Blätterteig').mouseout(
+			function () {
+				$("#pr12").stop().hide(150)
+			});
+
+		$('#node-MBudget-Cervelas').mouseover(
+			function () {
+				$("#pr13").stop().show(150)
+			});
+
+		$('#node-MBudget-Cervelas').mouseout(
+			function () {
+				$("#pr13").stop().hide(150)
+			});
+
+		$('#node-MClass-Choco-Drink').mouseover(
+			function () {
+				$("#pr14").stop().show(150)
+			});
+
+		$('#node-MClass-Choco-Drink').mouseout(
+			function () {
+				$("#pr14").stop().hide(150)
+			});
+
+		$('#node-MClass-JogCaramel').mouseover(
+			function () {
+				$("#pr15").stop().show(150)
+			});
+
+		$('#node-MClass-JogCaramel').mouseout(
+			function () {
+				$("#pr15").stop().hide(150)
+			});
+
+		$('#node-MClass-Penne').mouseover(
+			function () {
+				$("#pr16").stop().show(150)
+			});
+
+		$('#node-MClass-Penne').mouseout(
+			function () {
+				$("#pr16").stop().hide(150)
+			});
+
+		$('#node-MClass-Wiener-Waffel').mouseover(
+			function () {
+				$("#pr17").stop().show(150)
+			});
+
+		$('#node-MClass-Wiener-Waffel').mouseout(
+			function () {
+				$("#pr17").stop().hide(150)
+			});
+
+		$('#node-Noxana').mouseover(
+			function () {
+				$("#pr18").stop().show(150)
+			});
+
+		$('#node-Noxana').mouseout(
+			function () {
+				$("#pr18").stop().hide(150)
+			});
+
+		$('#node-Philadelphia-Kräuter').mouseover(
+			function () {
+				$("#pr19").stop().show(150)
+			});
+
+		$('#node-Philadelphia-Kräuter').mouseout(
+			function () {
+				$("#pr19").stop().hide(150)
+			});
+
+		$('#node-Quiche-Lorraine').mouseover(
+			function () {
+				$("#pr20").stop().show(150)
+			});
+
+		$('#node-Quiche-Lorraine').mouseout(
+			function () {
+				$("#pr20").stop().hide(150)
+			});
+
+		$('#node-Ruchbrot').mouseover(
+			function () {
+				$("#pr21").stop().show(150)
+			});
+
+		$('#node-Ruchbrot').mouseout(
+			function () {
+				$("#pr21").stop().hide(150)
+			});
+
+		$('#node-Tortilla-Chips-Chili').mouseover(
+			function () {
+				$("#pr22").stop().show(150)
+			});
+
+		$('#node-Tortilla-Chips-Chili').mouseout(
+			function () {
+				$("#pr22").stop().hide(150)
+			});
+
+		$('#node-Twix').mouseover(
+			function () {
+				$("#pr23").stop().show(150)
+			});
+
+		$('#node-Twix').mouseout(
+			function () {
+				$("#pr23").stop().hide(150)
+			});
+
+		$('#node-Valflora-Halbrahm').mouseover(
+			function () {
+				$("#pr24").stop().show(150)
+			});
+
+		$('#node-Valflora-Halbrahm').mouseout(
+			function () {
+				$("#pr24").stop().hide(150)
+			});
+
+		$('#node-Wassermelone-mini').mouseover(
+			function () {
+				$("#pr25").stop().show(150)
+			});
+
+		$('#node-Wassermelone-mini').mouseout(
+			function () {
+				$("#pr25").stop().hide(150)
+			});
+
+	}, 20);
+
+
+
+
+
+}); //READY
